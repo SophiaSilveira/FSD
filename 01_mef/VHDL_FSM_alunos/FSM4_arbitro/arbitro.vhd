8d453396852a38157629f7877426ea95d7884561
Sophia M Silveira <89554510+SophiaSilveira@users.noreply.github.com>
2023-10-24 12:01:26 +0000

    ex arbitro

--- 01_mef/VHDL_FSM_alunos/FSM4_arbitro/arbitro.vhd
+++ 01_mef/VHDL_FSM_alunos/FSM4_arbitro/arbitro.vhd
@@ -20,37 +20,56 @@ architecture a1 of arbitro is
 
 begin
 
+    seq: process(reset,clock)
+    begin
+        if reset = '1' then
+            EA <= idle;
+        elsif rising_edge(clock) then
+            EA <= PE;
+        end if;   
+    end process;
 
-    --
-    --  CONTROLE: MAQUINA DE ESTADOS
-    -- 
-    <<<<  FAZER AQUI A CODIFICAÇÃO DA MÁQUINA DE ESTADOS >>>
+    process(EA, req, release)
+    begin
+        case EA is
+            when idle =>
+                if req(0) = '1' or  req(1) = '1' or  req(2) = '1' or  req(3) = '1' 
+                    then PE <= sselect;
+                else PE <=  idle;
+                end if ;
+            when sselect => PE <= ack;
+            when ack => PE <= waiting;
+            when waiting => 
+                if release(conv_integer(sel))='1' then PE <= idle;
+                else PE <= waiting;
+                end if;
+        end case;    
+    end process;
 
 
+    grant(0)<= '1' when EA=ack and sel="00" else '0';
+    grant(1)<= '1' when EA=ack and sel="01" else '0';
+    grant(2)<= '1' when EA=ack and sel="10" else '0';
+    grant(3)<= '1' when EA=ack and sel="11" else '0';
 
-    grant(0)<='1' when EA=______ and sel="00" else '0';
-    grant(1)<=  ...
-    grant(2)<=  ...
-    grant(3)<=  ...
-    --
-    --  DADOS: REGISTRADORES E SAIDA
-    --     
-    process(...)
+
+    process(reset, clock)
     begin
         if reset = '1' then 
              sel<="00";
-        elsif rising_edge(...) then
+        elsif rising_edge(clock) then
            if EA= sselect  then
-
                     if req(CONV_INTEGER(sel+1)) = '1' then
                                 sel <= sel+1;
-                    ...
-
+                    elsif req(CONV_INTEGER(sel+2)) = '1' then
+                        sel <= sel+2;
+                    elsif req(CONV_INTEGER(sel+3)) = '1' then
+                            sel <= sel+3;                      
+                    elsif req(CONV_INTEGER(sel)) = '1' then
+                                sel <= sel;            
                     end if; 
-                    
            end if;
         end if;
     end process;
-
 
 end a1;
